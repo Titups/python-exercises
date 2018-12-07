98bd5e86cb8205e7a504630ae04a11beca930526
Titups <43752139+Titups@users.noreply.github.com>
2018-12-06 19:55:32 -0500

    Add files via upload

---
+++ Exercise 37 (1).py
@@ -0,0 +1,40 @@
+and:logic operator
+del:remove dictionary explicitly
+from:imports specific attributes 
+not:logic operator
+while:creates loops if true
+as:create alias while importing a module
+elif:conditional branching or decision making
+global: outside of function
+or: logic operator
+with:wrap execution of the block
+assert:used for debugging
+else:executes when expression is false
+if:used for boolean expressions
+pass: syntactically wanted but do not want to execute
+yield:used inside a function like a return statement
+break:terminates loop statement
+except:used for exceptions
+import:imports a module present in a search path
+print: prints sentences and words
+class:new user defined-class. represent a real world situation
+exec:allows execution
+in:evaluates to true
+raise:exceptions
+continue:causes loop to skip remainder and retest condition
+finally:close up resources or file streams
+is:operands are identical
+return:exits a function
+def:define a function
+for:executes statements multiple times
+lambda:can take many arguments, but return just one value
+try:exceptions
+    
+True:truth values comparison
+False:truth values comparison
+None:abscence of a value
+strings:created by enclosing characters with single, double, or triple quotes
+numbers: 1, 4.0, 30
+floats: 5.0, 9.0
+lists:created by placing items inside brackets
+    
